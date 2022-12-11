d38e043a7d172d8729e016c23126c13b0f6d6eb5
1y1c0c8 <f74096255@gs.ncku.edu.tw>
2022-12-12 05:05:59 +0800

    Upload: without 3.2

--- controller.py
+++ controller.py
@@ -1,15 +1,10 @@
 from PyQt5 import QtWidgets, QtGui, QtCore
 from PyQt5.QtWidgets import QFileDialog
 
-
 import os
-from PIL import Image
 import cv2
 import numpy as np
-from tkinter import filedialog
 import glob
-import time
-import utils
 
 from UI import Ui_MainWindow
 
@@ -56,15 +51,10 @@ class MainWindow_controller(QtWidgets.QMainWindow):
         self.ui.findDistortionButton.clicked.connect(self.printDistortion)
         self.ui.showResultButton.clicked.connect(self.showResult)
         self.ui.showWordsOnBoardButton.clicked.connect(self.showWordsOnBoard)
-
+        self.ui.shoWordsVerticallyButton.clicked.connect(self.Show_Words_Vertically)
         self.ui.showDisparityMapButton.clicked.connect(self.stereoDisparityMap)
     #Functions
     def loadFolder(self):
-
-        # self.dirPath = filedialog.askdirectory(initialdir="C:\\Users\kenny\PycharmProjects\ImageProcessingHw2", title="Select directory")
-        # self.ui.loadFolderLabel.setText(self.dirPath)
-        # self.dirPath += '/*.bmp'
-    #     ============================
         self.dirPath = QFileDialog.getExistingDirectory()
         self.ui.loadFolderLabel.setText(self.dirPath)
         self.dirPath = os.path.join(self.dirPath, "*.bmp")
@@ -102,11 +92,6 @@ class MainWindow_controller(QtWidgets.QMainWindow):
         blurred2 = cv2.GaussianBlur(img2, (5, 5), 0)
         self.canny2 = cv2.Canny(blurred2, 30, 150)
 
-        # height1 = (self.canny1.shape[0])*2
-        # width1 = (self.canny1.shape[1])*2
-        # height2 = (self.canny2.shape[0])*2
-        # width2 = (self.canny2.shape[1])*2
-
         self.canny1 = cv2.resize(self.canny1, (width1, height1))
         self.canny2 = cv2.resize(self.canny2, (width2, height2))
 
@@ -119,7 +104,6 @@ class MainWindow_controller(QtWidgets.QMainWindow):
     def countRing(self):
         img1 = cv2.imread(self.path1)
         gray1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
-        # blur = cv2.GaussianBlur(gray, (5, 5), 0)
         blur1 = cv2.medianBlur(gray1, 5)
         cimg1 = cv2.cvtColor(blur1, cv2.COLOR_GRAY2BGR)
 
@@ -137,9 +121,7 @@ class MainWindow_controller(QtWidgets.QMainWindow):
 
         img2 = cv2.imread(self.path2)
         gray2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
-        # blur = cv2.GaussianBlur(gray, (5, 5), 0)
         blur2 = cv2.medianBlur(gray2, 5)
-        cimg2 = cv2.cvtColor(blur2, cv2.COLOR_GRAY2BGR)
 
         circles2 = cv2.HoughCircles(blur2, cv2.HOUGH_GRADIENT, 1, 120, param1=100, param2=30, minRadius=0, maxRadius=0)
         circles2 = np.uint16(np.around(circles2))
@@ -168,7 +150,6 @@ class MainWindow_controller(QtWidgets.QMainWindow):
             ny = 8
             nx = 11
             ret, corners = cv2.findChessboardCorners(gray, (nx, ny), None)
-            # print('ret: ', ret)
 
             if ret == True:
                 cv2.drawChessboardCorners(chess_board_image, (nx, ny), corners, ret)
@@ -270,7 +251,11 @@ class MainWindow_controller(QtWidgets.QMainWindow):
 
     def getEndpointsCoordinate(self, line):
         # return coordinates of a line's 2 endpoints
-        co = ((int(line[0][0]), int(line[0][1])), (int(line[1][0]), int(line[1][1])))
+        co = (
+            (int(line[0][0]), int(line[0][1])),
+            (int(line[1][0]), int(line[1][1]))
+        )
+
         return co
 
     def getLineList(self, alphabet):
@@ -325,7 +310,9 @@ class MainWindow_controller(QtWidgets.QMainWindow):
         lineList = self.getLineList(alphabet)
 
         for line in lineList:
+            # print('before getEndpointsCoordinate')
             COs = self.getEndpointsCoordinate(line)
+            # print('after getEndpointsCoordinate')
             base = self.base[alphabetIndex]
             print(COs[0])
             print(COs[1])
@@ -347,9 +334,6 @@ class MainWindow_controller(QtWidgets.QMainWindow):
         # 3.Derive the shape of the “Word” by using the provided library
         self.fs = cv2.FileStorage('Q3_Image/Q2_lib/alphabet_lib_onboard.txt', cv2.FILE_STORAGE_READ)
 
-
-        self.string = self.ui.lineEdit.text()[:6].upper()
-
         chess_images = glob.glob(self.dirPath)
 
         for imgIndex in range(len(chess_images)):
@@ -364,7 +348,7 @@ class MainWindow_controller(QtWidgets.QMainWindow):
                 alphabetIndex = alphabetIndex+1
 
             cv2.imshow('result', img)
-            cv2.waitKey(500)
+            cv2.waitKey(1000)
             cv2.destroyWindow('result')
             print('==========================')
 
@@ -372,18 +356,140 @@ class MainWindow_controller(QtWidgets.QMainWindow):
         cv2.destroyAllWindows()
 
 
-    # def showWordsVertically():
+    def showWordsVertically(self):
+        # # termination criteria
+        # criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+        #
+        # # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+        # objp = np.zeros((11 * 8, 3), np.float32)
+        # objp[:, :2] = np.mgrid[0:8, 0:11].T.reshape(-1, 2)
+        #
+        # # axis = np.float32([[3, 3, -3], [1, 1, 0], [3, 5, 0], [5, 1, 0]]).reshape(-1, 3)
+        # axis = np.float32([[5, 3, -3], [7, 1, 0], [3, 3, 0], [7, 5, 0]]).reshape(-1, 3)
+        #
+        # # Arrays to store object points and image points from all the images.
+        # objpoints = []  # 3d point in real world space
+        # imgpoints = []  # 2d points in image plane
+        #
+        # print(self.dirPath)
+        # chess_images = glob.glob(self.dirPath)
+        # # Select any index to grab an image from the list
+        # for i in range(len(chess_images)):
+        #     # Read in the image
+        #     image = cv2.imread(chess_images[i])
+        #     # Convert to grayscale
+        #     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        #     # Find the chessboard corners
+        #     ret, corners = cv2.findChessboardCorners(gray, (8, 11), None)
+        #
+        #     if ret == True:
+        #         objpoints.append(objp)
+        #         # objp = 8 * 11 objpoints (x, y, z)
+        #         corners2 = cv2.cornerSubPix(gray, corners, (7, 7), (-1, -1), criteria)
+        #         imgpoints.append(corners2)
+        #         # corner2 = each object point on 2D image (x, y)
+        #
+        #         # gray.shape[::-1] = (2048, 2048)
+        #         ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, (2048, 2048), None, None)
+        #
+        #         # project 3D points to image plane
+        #         imgpts, jac = cv2.projectPoints(axis, rvecs[i], tvecs[i], mtx, dist)
+        #
+        #         def draw(image, imgpts):
+        #             image = cv2.line(image, tuple(imgpts[0].ravel()), tuple(imgpts[1].ravel()), (0, 0, 255), 5)
+        #             image = cv2.line(image, tuple(imgpts[0].ravel()), tuple(imgpts[2].ravel()), (0, 0, 255), 5)
+        #             image = cv2.line(image, tuple(imgpts[0].ravel()), tuple(imgpts[3].ravel()), (0, 0, 255), 5)
+        #             image = cv2.line(image, tuple(imgpts[1].ravel()), tuple(imgpts[2].ravel()), (0, 0, 255), 5)
+        #             image = cv2.line(image, tuple(imgpts[1].ravel()), tuple(imgpts[3].ravel()), (0, 0, 255), 5)
+        #             image = cv2.line(image, tuple(imgpts[2].ravel()), tuple(imgpts[3].ravel()), (0, 0, 255), 5)
+        #             return image
+        #
+        #         img = draw(image, imgpts)
+        #
+        #         cv2.imwrite('%s_v.jpg' % i, img)
+        #         img = cv2.resize(img, (1024, 1024), interpolation=cv2.INTER_AREA)
+        #         cv2.namedWindow('img')
+        #         cv2.imshow('img', img)
+        #         cv2.waitKey(1000)
+
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+    def Show_Words_Vertically(self):  # input for the text
+        # # 1.Calibrate: get Intrinsic(self.mtx), distortion(self.dist), extrinsic(self.ext)
+        # self.findParam()
+        #
+        # self.string = self.ui.lineEdit.text()[:6].upper()
+        #
+        # self.fs = cv2.FileStorage('Q3_Image/Q2_lib/alphabet_lib_vertical.txt', cv2.FILE_STORAGE_READ)
+        #
+        # chess_images = glob.glob(self.dirPath)
+        #
+        # for imgIndex in range(len(chess_images)):
+        #
+        #     alphabetIndex = 0
+        #
+        #     img = cv2.imread(chess_images[imgIndex])
+        #
+        #     # ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(obj, img, (2048, 2048), None, None)
+        #
+        #     for imgIndex in range(len(self.string)):
+        #         file = cv2.FileStorage('Q3_Image/Q2_lib/alphabet_lib_vertical.txt', cv2.FILE_STORAGE_READ)
+        #
+        #         ch = np.float32(file.getNode(input[imgIndex].upper()).mat()).reshape(-1, 3)
+        #         for k in range(len(ch)):  # Change x and y
+        #             temp = ch[k][0]
+        #             ch[k][0] = ch[k][1]
+        #             ch[k][1] = temp
+        #         for k in range(len(ch)):  # Upside down
+        #             if ch[k][0] == 2:
+        #                 ch[k][0] = 0
+        #             elif ch[k][0] == 0:
+        #                 ch[k][0] = 2
+        #         if imgIndex == 0:
+        #             for k in range(len(ch)):
+        #                 ch[k][1] += 8
+        #         elif imgIndex == 1:
+        #             for k in range(len(ch)):
+        #                 ch[k][1] += 4
+        #         elif imgIndex == 2:
+        #             pass
+        #         elif imgIndex == 3:
+        #             for k in range(len(ch)):
+        #                 ch[k][1] += 8
+        #                 ch[k][0] += 4
+        #         elif imgIndex == 4:
+        #             for k in range(len(ch)):
+        #                 ch[k][1] += 4
+        #                 ch[k][0] += 4
+        #         elif imgIndex == 5:
+        #             for k in range(len(ch)):
+        #                 ch[k][0] += 4
+        #
+        #         print(ch)
+        #         imgpts, jac = cv2.projectPoints(ch, self.rvecs[imgIndex], self.tvecs[imgIndex], self.mtx, self.dist)
+        #         for k in range(0, len(ch), 2):
+        #             image = cv2.line(image, tuple(imgpts[k].ravel()), tuple(imgpts[k + 1].ravel()), (0, 0, 255), 5)
+        #
+        #     cv2.namedWindow("Augmented Reality", cv2.WINDOW_GUI_EXPANDED)
+        #     # image = cv2.resize(image, (image.shape[0]//4, image.shape[1]//4))
+        #     cv2.imshow("Augmented Reality", image)
+        #     cv2.waitKey(1000)
+        #
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
+
 
     def stereoDisparityMap(self):
-        # imgL = cv2.imread(self.path1)
-        # imgR = cv2.imread(self.path2)
-        #
-        # stereo = cv2.StereoBM_create(numDisparities = 16, blockSize = 15)
-        # print('Created')
-        # disparity = stereo.compute(imgL, imgR)
-        # print('Computed')
-        #
-        # cv2.imshow('Result', disparity)
+        img1 = cv2.imread(self.path1)
+        img2 = cv2.imread(self.path2)
+
+        stereo = cv2.StereoBM_create(numDisparities = 256, blockSize=25)
+        img1_gray = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+        img2_gray = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+        disparity = stereo.compute(img1_gray, img2_gray).astype(np.float32) /16.0
+        disparity_show = cv2.normalize(disparity, None, 0, 1, cv2.NORM_MINMAX)
+        cv2.imshow('Result', disparity_show)
 
         cv2.waitKey(0)
         cv2.destroyAllWindows()
